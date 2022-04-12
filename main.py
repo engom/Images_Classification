b669aedfc4f6eb84b5cb68d5dbe9979f4706c898
Elhadji NGOM <ngomel.ehn@gmail.com>
2022-04-12 11:20:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 # import codes
 import src.data.data_loader as loader
 import src.models.cnn as modeler
-
+import src.data.processing_data as processor
 # link of source data
 src = 'https://test-backet-dsti.s3.amazonaws.com/kagglecatsanddogs_3367a.zip'
 # path of destination director
@@ -27,42 +27,7 @@ if __name__=='__main__':
   # load and unzip
   loader.download_zip(dti, src)
   # data processing
-  num_skipped = 0
-  for folder_name in ("Cat", "Dog"):
-      folder_path = os.path.join(f"{dti}/PetImages", folder_name)
-      for fname in os.listdir(folder_path):
-          fpath = os.path.join(folder_path, fname)
-          try:
-              fobj = open(fpath, "rb")
-              is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
-          finally:
-              fobj.close()
-
-          if not is_jfif:
-              num_skipped += 1
-              # Delete corrupted image
-              os.remove(fpath)
-
-  print("Deleted %d images" % num_skipped)
-
-  image_size = (180, 180)
-  batch_size = 32
-  train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-                                                                f"{dti}/PetImages",
-                                                                validation_split=0.2,
-                                                                subset="training",
-                                                                seed=1337, 
-                                                                image_size=image_size,
-                                                                batch_size=batch_size,
-                                                                )
-  val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-                                                              f"{dti}/PetImages",
-                                                              validation_split=0.2,
-                                                              subset="validation",
-                                                              seed=1337,
-                                                              image_size=image_size,
-                                                              batch_size=batch_size,
-                                                              )
+  processor.data_pressor(dti)
   # build model
   model = modeler.make_model(input_shape=image_size + (3,), num_classes=2) 
   # train model
