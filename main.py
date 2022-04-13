ca356fbd7375eb98181dfc758105a03f04f83b17
Elhadji NGOM <ngomel.ehn@gmail.com>
2022-04-13 23:52:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,23 @@ src = 'https://test-backet-dsti.s3.amazonaws.com/kagglecatsanddogs_3367a.zip'
 # path of destination director
 dst = '/home/ubuntu/project/classification/data/raw'
 
+def push_mlflow(TRACKING_URI, metric_name, value, run_name = 'Default'):
+    """
+    Function to send information to a given mlflow client.
+    Args:
+    TRACKING_URI (str): IP address info of the client (http://52.215.94.6:5000/)
+    metric_name (str): Name of the metric to save
+    value (float): Value of the metric
+    run_name (str): Name of the run
+    Returns:
+    None
+    """
+    mlflow.tracking.set_tracking_uri(TRACKING_URI)
+    with mlflow.start_run(run_name = run_name) as run:
+        mlflow.log_metric(metric_name, value)
+# Track the model with mlflow
+TRACKING_URI = "http://44.198.184.229:5000/"
+        
 # main program starts
 if __name__=='__main__':
   # load and unzip
@@ -52,10 +69,25 @@ if __name__=='__main__':
                 loss="binary_crossentropy",
                 metrics=["accuracy"],)
   model.fit(train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds, verbose=4)
+  
   # save model to s3
   my_bucket.upload_file(model.save('model.h5'))
   
-  # Track the model with mlflow
-  TRACKING_URI = "http://44.198.184.229:5000/"
-  mlflow.tracking.set_tracking_uri(TRACKING_URI)
-  mlflow.start_run()
+  #keras.utils.plot_model(model, show_shapes=True)
+  
+  ## mlflow 
+  run_name = 'Default'
+  metric_name='accuracy'
+  value = 10
+
+  #push_mlflow(TRACKING_URI, metric_name, value ,run_name = run_name)
+
+  ## SNS
+  client = boto3.client('sns', region_name='us-east-1')
+
+  response = client.publish(
+                TopicArn='arn:aws:sns:eu-west-2:321132792081:AWS_Topic',
+                Message='Training done',
+                Subject='Classification training'
+                )
+   print(response)
